1697c29c9f3daa901ff0edf4238a43044e138e91
Swapnil Hadge <hadge.swapnil2012@gmail.com>
2018-07-25 00:20:58 +0530

    fronend

---
+++ frontend/src/app/common/constants.ts
@@ -0,0 +1,10 @@
+
+export class Constant {
+    public static apiEndpoint = 'http://localhost:3000/api';
+
+    public static register = Constant.apiEndpoint + '/UserModels';
+    public static login = Constant.apiEndpoint + '/UserModels/login';
+    public static getInterestList = Constant.apiEndpoint + '/AOTExpressions';
+    public static getList = Constant.apiEndpoint + '/AOTExpressions/getInterestList';
+    public static signOut = Constant.apiEndpoint + '/UserModels/logout';
+}
