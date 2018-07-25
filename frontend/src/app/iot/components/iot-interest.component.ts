5118cbd2f077454ceb081928c0564f0cfd8c6c5e
Swapnil Hadge <hadge.swapnil2012@gmail.com>
2018-07-25 11:49:06 +0530

    validations and date issue fixed before sign in

--- frontend/src/app/iot/components/iot-interest.component.ts
+++ frontend/src/app/iot/components/iot-interest.component.ts
@@ -31,18 +31,7 @@ export class IOTInterestComponent implements OnInit {
     constructor(private router: Router, private toasterService: ToasterService, private iotservice: IOTservice) {
 
         if (localStorage.getItem("access_Token") && localStorage.getItem("access_Token") != 'undefined') {
-            this.iotservice.getUserDetailsById()
-                .subscribe(
-                (response) => {
-                    this.IsAuthenticate = true;
-                    this.LoggedInUser = JSON.parse(response._body);
-                },
-                (err) => {
-                    console.error(err);
-                },
-                () => {
-                }
-                );
+            this.get_expression();
         }
     }
 
@@ -65,6 +54,17 @@ export class IOTInterestComponent implements OnInit {
     }
 
     login() {
+        if(!this.model.mobile){
+            this.toasterService.pop('error', 'Mobile  can not be empty');
+            return;
+        }
+
+        if(!this.model.password){
+            this.toasterService.pop('error', 'Password  can not be empty');
+            return;
+        }
+
+
         let conf: any = {};
         conf.mobile = this.model.mobile;
         conf.password = this.model.password;
@@ -93,7 +93,7 @@ export class IOTInterestComponent implements OnInit {
             (err) => {
                 console.error(err);
                 this.toasterService.clear();
-                this.toasterService.pop('error', 'Logged in failed');
+                this.toasterService.pop('error', 'Log in failed');
             },
             () => {
             }
@@ -106,22 +106,22 @@ export class IOTInterestComponent implements OnInit {
         this.IsAuthenticate = false;
 
         this.iotservice.signOut(token)
-        .subscribe(
-        (response) => {
-            this.toasterService.clear();
-            this.toasterService.pop('success', ' Signed out successfully');
-            this.ngOnInit();
-        },
-        (err) => {
-           // console.error(err);
-           // this.toasterService.pop('error', ' Signed out failed');
-        },
-        () => {
-        }
-        );
+            .subscribe(
+            (response) => {
+                this.toasterService.clear();
+                this.toasterService.pop('success', ' Signed out successfully');
+                this.ngOnInit();
+            },
+            (err) => {
+                // console.error(err);
+                // this.toasterService.pop('error', ' Signed out failed');
+            },
+            () => {
+            }
+            );
 
-       
-       
+
+
     }
 
     write_expression() {
@@ -131,7 +131,7 @@ export class IOTInterestComponent implements OnInit {
             this.model = {};
             return;
         }
-        if(this.model.expression == 'undefined'){
+        if (this.model.expression == 'undefined') {
             this.model = {};
             return;
         }
@@ -161,12 +161,14 @@ export class IOTInterestComponent implements OnInit {
                 // this.InterestList = JSON.parse(response._body);
                 this.InterestList = JSON.parse(response._body).body;
                 this.InterestList.forEach(interest => {
-                    if (interest.userModelId == JSON.parse(localStorage.getItem('user')).userId) {
-                        interest.IsLoggedInUser = true;
-                    } else {
-                        interest.IsLoggedInUser = false;
+                    if (JSON.parse(localStorage.getItem('user'))) {
+                        if (interest.userModelId == JSON.parse(localStorage.getItem('user')).userId) {
+                            interest.IsLoggedInUser = true;
+                        } else {
+                            interest.IsLoggedInUser = false;
+                        }
                     }
-                    interest.date = new Date(interest.date).getMonth() + "/" + new Date(interest.date).getDay() + "/"
+                    interest.date = new Date(interest.date).getMonth() + 1 + "/" + new Date(interest.date).getDate() + "/"
                         + new Date(interest.date).getFullYear() + " " + new Date(interest.date).getHours() + ":" + new Date(interest.date).getMinutes()
                         + ":" + new Date(interest.date).getSeconds();
                 });
